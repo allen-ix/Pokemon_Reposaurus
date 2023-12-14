b2febde65fd1fdb48a9b74ca6dbc70a8981d0918
John Allen Dugelio <dugeliojohnallen@gmail.com>
2023-12-14 18:49:41 +0800

    x sv

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -38,3 +38,17 @@ path('trainer_list/', TrainerList.as_view(), name='trainer-list'),
 path('pokemon-card-list/', PokemonCardList.as_view(), name='pokemon-card-list'),
 path('collections/', CollectionList.as_view(), name='collection-list'),
 ]
+
+from django.urls import path
+from cardquest.views import HomePageView
+from cardquest import views
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('', views.HomePageView.as_view(), name='home'),
+]
+
+from cardquest.views import HomePageView, TrainerList
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('', views.HomePageView.as_view(), name='home'),
+path('trainer_list', TrainerList.as_view(), name='trainer-list'),]
