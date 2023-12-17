ddc9809ca9217bd6a2d0b2d25ad3cd9bfb5eee34
John Allen Dugelio <dugeliojohnallen@gmail.com>
2023-12-17 13:51:50 +0800

    Revert " x sv"
    This reverts commit b2febde65fd1fdb48a9b74ca6dbc70a8981d0918.

--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -3,16 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.list import ListView
 from cardquest.models import PokemonCard, Trainer , Collection
-from django.views.generic.list import ListView
-from cardquest.models import PokemonCard
 
-class HomePageView(ListView):
-    model = PokemonCard
-    context_object_name = 'home'
-    template_name = "home.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
 class HomePageView(ListView):
     model = PokemonCard
     context_object_name = 'home'
@@ -53,12 +44,4 @@ def my_view(request):
         print(f"{card.name}: {card.image_url}")
 
     return render(request, 'pokemon-cards.html', {'pokemon_cards': pokemon_cards})
-
-from cardquest.models import PokemonCard, Trainer
-class TrainerList(ListView):
-    model = Trainer
-    context_object_name = 'trainer'
-    template_name = 'trainers.html'
-    paginate_by = 15
-
 
